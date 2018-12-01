fb67c319770df367c66afe09198a8106578a86e4
Rob Baillie <robert.baillie@makepositive.com>
2018-12-01 19:31:03 +0000

    Switched off debug

--- index.php
+++ index.php
@@ -4,16 +4,10 @@
 	error_reporting(E_ALL);
 	
 	set_time_limit ( 60*60 ); // 1 hour 
-	$debug = 2;
+	$debug = 0;
 
 	require_once( 'powerof10.php' );
 	require_once( 'keyfunctions.php' );
-/*
-	$athletes = readAthletes( $debug );
-echo( 'about to read athlete results' );
-	var_dump( readAthleteResults( 2, $athletes[0] ) );
-echo( 'done' );
-*/
 
 	$return = [ 'error' => 'A valid mode was not specified' ];
 
